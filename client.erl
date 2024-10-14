b6b66a12ccc61b0c3232176efbf3d81dd3ccf069
yusnof <yusnofal@gmail.com>
2024-10-14 13:25:07 +0200

    simple implemnation of server and client

--- client.erl
+++ client.erl
@@ -4,9 +4,13 @@
 % This record defines the structure of the state of a client.
 % Add whatever other fields you need.
 -record(client_st, {
-    gui, % atom of the GUI process
-    nick, % nick/username of the client
-    server % atom of the chat server
+    % atom of the GUI process
+    gui,
+    % nick/username of the client
+    nick,
+    % atom of the chat server
+    server,
+    channel
 }).
 
 % Return an initial state record. This is called from GUI.
@@ -15,7 +19,8 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
     #client_st{
         gui = GUIAtom,
         nick = Nick,
-        server = ServerAtom
+        server = ServerAtom,
+        channel = []
     }.
 
 % handle/2 handles each kind of request from GUI
@@ -28,45 +33,47 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Join channel
 handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "join not implemented"}, St} ;
-
+    case genserver:request(St#client_st.server, {join, self(), St#client_st.nick, Channel}) of
+        ok ->
+            TempList = [Channel | St#client_st.channel],
+            {reply, ok, St#client_st{channel = TempList}};
+        noOkey -> {reply, "NOOOO", St}
+    end;
 % Leave channel
 handle(St, {leave, Channel}) ->
     % TODO: Implement this function
     % {reply, ok, St} ;
-    {reply, {error, not_implemented, "leave not implemented"}, St} ;
+    case lists:member(Channel , St#client_st.channel) of 
+        true -> 
+            TempList = lists:delete(Channel , St#client_st.channel),
+            genserver:request(St#client_st.server, {quit,St#client_st.nick,Channel}),
+        {reply, {error, not_implemented, "leave not implemented"}, St};
+        false -> ok; 
 
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
     % TODO: Implement this function
     % {reply, ok, St} ;
-    {reply, {error, not_implemented, "message sending not implemented"}, St} ;
-
+    {reply, {error, not_implemented, "message sending not implemented"}, St};
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
 handle(St, {nick, NewNick}) ->
-    {reply, ok, St#client_st{nick = NewNick}} ;
-
+    {reply, ok, St#client_st{nick = NewNick}};
 % ---------------------------------------------------------------------------
 % The cases below do not need to be changed...
 % But you should understand how they work!
 
 % Get current nick
 handle(St, whoami) ->
-    {reply, St#client_st.nick, St} ;
-
+    {reply, St#client_st.nick, St};
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
-    gen_server:call(GUI, {message_receive, Channel, Nick++"> "++Msg}),
-    {reply, ok, St} ;
-
+    gen_server:call(GUI, {message_receive, Channel, Nick ++ "> " ++ Msg}),
+    {reply, ok, St};
 % Quit client via GUI
 handle(St, quit) ->
     % Any cleanup should happen here, but this is optional
-    {reply, ok, St} ;
-
+    {reply, ok, St};
 % Catch-all for any unhandled requests
 handle(St, Data) ->
-    {reply, {error, not_implemented, "Client does not handle this command"}, St} .
+    {reply, {error, not_implemented, "Client does not handle this command"}, St}.
--- server.erl
+++ server.erl
@@ -1,18 +1,56 @@
 -module(server).
--export([start/1,stop/1]).
+-export([start/1, stop/1]).
+
+-record(serverState, {
+    users,
+    channels
+}).
+
+initial_state() ->
+    #serverState{
+        users = [],
+        channels = []
+    }.
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    not_implemented.
+    genserver:start(ServerAtom, initial_state(), handle/2).
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+    genserver:stop(ServerAtom).
+
+handle(State, {join, Pid, NewUserName, Channel}) ->
+    case lists:member({NewUserName, Pid}, State#serverState.users) of
+        true ->
+            {reply, {error, already_in_channel}, State};
+        false ->
+            %% Add the user to the channel
+            NewUsers = [{NewUserName, Pid} | State#serverState.users],
+            NewState = State#serverState{users = NewUsers},
+            {reply, {ok, Channel}, NewState}
+    end;
+
+
+%function that handles the quitComand and that replyes with ok
+% and with the state without the deleted user
+handle(State, {quit, UserName}) ->
+    TempList = lists:delete(UserName, State#serverState.users),
+    {reply, ok, State#serverState{users = TempList}}; 
+
+%function that handles changing NickName 
+handle(State,{nick, OldUserName, NewUserName})-> 
+    case lists:member(NewUserName, State#serverState.users) of
+        true when OldUserName =:= NewUserName ->
+            {reply, ok, State};
+        false ->
+            TempList = [NewUserName | lists:delete(OldUserName, State#serverState.users)],
+            {reply, ok, State#serverState{users=TempList}}
+        end;
+
+
+%ToDo catch other commands with some type of exeption
+handle(State, _ ) ->
+    {reply, {error, not_implemented}, State}.
